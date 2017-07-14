7e4b063d59e45cd337fee1c639ce8821bc60f048
thiagomsdev <thiagomsdev@gmail.com>
2017-07-14 17:49:24 +0100

    Abstract Factory

--- Creational Patterns/Abstract Factory/C#/Abstract Factory/Client.cs
+++ Creational Patterns/Abstract Factory/C#/Abstract Factory/Client.cs
@@ -1,9 +1,5 @@
 ﻿using Abstract_Factory.Factories;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Abstract_Factory
 {
--- Creational Patterns/Abstract Factory/C#/Abstract Factory/Factories/EUAPlayerFactory.cs
+++ Creational Patterns/Abstract Factory/C#/Abstract Factory/Factories/EUAPlayerFactory.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Abstract_Factory.Models;
+﻿using Abstract_Factory.Models;
 using static Abstract_Factory.Models.Country;
 
 namespace Abstract_Factory.Factories
